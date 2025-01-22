b4b480d0048ae793ed0075880993a3005a4ac5f3
Tharun Thangavel <tharunthangavel@outlook.com>
2025-01-22 18:16:33 -0500

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iostream>
+#include "midifile/include/MidiFile.h"
+#include "midifile/include/midi.h"
+
+using namespace std;
+
+int main() {
+	saveFile();
+
+
+
+	return 0;
+}
